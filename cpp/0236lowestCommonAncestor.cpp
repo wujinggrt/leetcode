9287b3735e694b340f105b6379cc0271c9d42d13
wujing <wujinggrt@qq.com>
2024-06-09 23:10:48 +0800

    236 better solution

--- cpp/0236lowestCommonAncestor.cpp
+++ cpp/0236lowestCommonAncestor.cpp
@@ -1,3 +1,29 @@
+// 最优解
+class Solution {
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == p || root == q) {
+      // 其中之一是root，立即返回
+      // 或者是递归时遇到了p或q，立即返回。此时有两种情况。
+      // 1）如果另一个节点在root的子树，那么root就是lowest公共祖先，在递归时候，
+      // 其他分支节点都不会包含p和q，都是nullptr，此root能够确保返回给最外层调用。
+      // 2）如果另一个节点不在root的子树，那么当前解决不了，只能抛给上一层调用者查看。
+      // 此题保证了p和q会存在树中，所以不会出现最终返回的根不包含两个节点情况。此corner
+      // case无需考虑。
+      return root;
+    }
+    auto* left = lowestCommonAncestor(root->left, p, q);
+    auto* right = lowestCommonAncestor(root->right, p, q);
+    if (left && right) {
+      return root;  // 分别分布在两侧，是我们想要的结果
+    }
+    // 如果当前root下，1）左右子树不包含p或q，会返回nullptr
+    // 2）如果仅仅包含一个，那么此root还不是公共祖先，返回即可，回溯给caller处理。
+    return left ? left : right;
+  }
+};
+#if 0
+// 次优解
 class Solution {
   bool walkThroughForBuilding(vector<TreeNode*>& path, TreeNode* root,
                               TreeNode* target) {
@@ -42,3 +68,4 @@ class Solution {
     return p_path[min_len - 1];  // 返回较小者的最后一个
   }
 };
+#endif
