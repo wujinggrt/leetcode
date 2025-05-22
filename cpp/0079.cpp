7921c89486650a91763135a9e149d81a80ee0772
wujing <wujinggrt@qq.com>
2025-05-22 18:11:45 +0800

    79

--- cpp/0079.cpp
+++ cpp/0079.cpp
@@ -1,66 +1,58 @@
 class Solution {
- public:
-  struct Position {
-    int x;
-    int y;
-  };
-
-  bool bfs(const vector<vector<char>>& board, string_view word,
-           const int startX, const int startY) const {
-    const int m = board.size();
-    const int n = board[0].size();
-    vector<vector<uint8_t>> isVisited(m * n, 0);
-    struct Node {
-      int x;
-      int y;
-      string_view::const_iterator wordIndex;
-    };
-    std::deque<Node> candidates;
-    candidates.push_back({startX, startY, word.cbegin()});
-    while (!candidates.empty()) {
-      const auto [x, y, it] = candidates.front();
-      candidates.pop_front();
-      if (x == -1 || y == -1 || x >= m || y >= n || isVisited[x][y] == 1) {
-        continue;
-      }
-      isVisited[x][y] = 1;
-      if () }
-  }
-
-  bool dfs(const vector<vector<char>>& board, string_view word,
-           const int startX, const int startY,
-           string_view::const_iterator it) const {}
-
-  bool exist(vector<vector<char>>& board, string word) {
-    const int m = board.size();
-    const int n = board[0].size();
-    vector<vector<uint8_t>> isVisited(m * n, 0);
-    vector<Position> frontPositions;
-    vector<Position> backPositions;
-    for (int i = 0; i != m; ++i) {
-      for (int j = 0; j != n; ++j) {
-        if (board[i][j] == word.front()) {
-          frontPositions.push_back({i, j});
-        }
-        if (board[i][j] == word.back()) {
-          backPositions.push_back({i, j});
-        }
-      }
-    }
-    if (frontPositions.empty() || backPositions.empty()) {
-      return false;
-    }
-    if (backPositions.size() < frontPositions.size()) {
-      std::swap(backPositions, frontPositions);
-      std::reverse(word.begin(), word.end());
-    }
-    std::deque<Position> candidates;
-    candidates.push_back(frontPositions.back());
-    frontPositions.pop_back();
-    while (!candidates.empty()) {
-      const auto [x, y] = candidates.front();
-      candidates.pop_front();
-      isVisited[x][y] = 1;
-    }
-  }
-};
+    public:
+     struct Position {
+       int x;
+       int y;
+     };
+   
+     bool dfs(const vector<vector<char>>& board, vector<vector<char>>& isVisited,
+              const int x, const int y,
+              string::const_iterator it, string::const_iterator end) const {
+       if (it == end) {
+           return true;
+       }        
+       const int m = board.size();
+       const int n = board[0].size();
+       if (x == -1 || y == -1 || x == m || y == n || isVisited[x][y] == '#' || board[x][y] != *it) {
+           return false; // reached boundary or mismatched
+       }
+       isVisited[x][y] = '#';
+       bool isMatched = dfs(board, isVisited, x - 1, y, it + 1, end);
+       if (!isMatched) isMatched = dfs(board, isVisited, x + 1, y, it + 1, end);
+       if (!isMatched) isMatched = dfs(board, isVisited, x, y - 1, it + 1, end);
+       if (!isMatched) isMatched = dfs(board, isVisited, x, y + 1, it + 1, end);
+       isVisited[x][y] = '*';
+       return isMatched; 
+       }
+   
+     bool exist(vector<vector<char>>& board, string word) {
+       const auto m = board.size();
+       const auto n = board[0].size();
+       vector<Position> frontPositions;
+       vector<Position> backPositions;
+       for (int i = 0; i != m; ++i) {
+         for (int j = 0; j != n; ++j) {
+           if (board[i][j] == word.front()) {
+             frontPositions.push_back({i, j});
+           }
+           if (board[i][j] == word.back()) {
+             backPositions.push_back({i, j});
+           }
+         }
+       }
+       if (frontPositions.empty() || backPositions.empty()) {
+         return false;
+       }
+       if (backPositions.size() < frontPositions.size()) {
+         std::swap(backPositions, frontPositions);
+         std::reverse(word.begin(), word.end());
+       }
+       vector<vector<char>> isVisited(m, vector<char>(n, '*'));
+       for (const auto& pos: frontPositions) {
+           if (dfs(board, isVisited, pos.x, pos.y, word.cbegin(), word.cend())) {
+               return true;
+           }
+       }
+       return false;
+     }
+   };
